e77e1083ee56a545166e1ef0af397735379a51b2
CarpalEnderman <miguelkjesus05@gmail.com>
2023-05-29 23:25:20 +0100

    efgetr

--- devrun.js
+++
@@ -1,8 +0,0 @@
-const realms = require("./index");
-require("dotenv").config();
-const { MS_EMAIL, MS_PASSWORD } = process.env;
-
-realms
-  .login(MS_EMAIL, MS_PASSWORD)
-  .then((user) => user.getRealms())
-  .then(console.log);
